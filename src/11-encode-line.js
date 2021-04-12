4fbfbade34b9caa55444ac1250f359b759dc9815
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-04-13 01:12:43 +0200

    feat: 11 encode finish

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let count = 1;
+  for (let index = 0; index < str.length; index++) {
+    const element = str[index];
+    const nextElement = str[index + 1];
+    if (element === nextElement) {
+      count++;
+      /* eslint-disable-next-line */
+      continue;
+    }
+    if (count > 1) {
+      result += count;
+      count = 1;
+    }
+    result += element;
+  }
+  return result;
 }
 
 module.exports = encodeLine;
