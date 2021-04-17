e2a2949854b0b7ea49fb0c4a110198f3a37a2d24
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-04-17 15:52:27 +0200

    feat:  binary serch index of element  10task completed

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,21 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let start = 0;
+  let end = array.length - 1;
+
+  while (start < end) {
+    let mid = Math.floor(start + end / 2);
+    if (array[mid] === value) {
+      return mid;
+    } if (array[mid] < value) {
+      start = mid++;
+    } else {
+      end = mid--;
+    }
+  }
+  return false;
 }
 
 module.exports = findIndex;
